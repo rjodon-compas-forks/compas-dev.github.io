fdad5554524edc8538af9c748c1e9f1080cfd254
brgcode <brg@arch.ethz.ch>
2019-07-16 19:58:41 +0200

    dunno

--- _source/conf.py
+++ _source/conf.py
@@ -4,9 +4,7 @@
 #
 # needs_sphinx = '1.0'
 
-
 import sphinx_compas_theme
-
 
 # -- General configuration ------------------------------------------------
 
@@ -30,60 +28,9 @@ add_module_names = True
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'matplotlib.sphinxext.plot_directive',
 ]
-
-# autodoc options
-
-autodoc_default_flags = [
-    'undoc-members',
-    'private-members',
-    'special-members',
-    'show-inheritance',
-]
-
-autodoc_member_order = 'alphabetical'
-
-# autosummary options
-
-autosummary_generate = True
-
-# napoleon options
-
-napoleon_google_docstring = True
-napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = True
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = False
-napoleon_use_rtype = False
-
-# plot options
-
-# plot_include_source
-# plot_pre_code
-# plot_basedir
-# plot_formats
-# plot_rcparams
-# plot_apply_rcparams
-# plot_working_directory
-# plot_template
-
-plot_html_show_source_link = False
-plot_html_show_formats = False
-
-# intersphinx options
-
-intersphinx_mapping = {'python': ('https://docs.python.org/', None)}
-
 
 # -- Options for HTML output ----------------------------------------------
 
